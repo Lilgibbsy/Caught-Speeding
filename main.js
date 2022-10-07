68d1fa4db74df592375b0a02efac4bfcce802b58
Lilgibbsy <owen@gibbsbunch.com>
2022-10-07 14:14:05 -0600

    Finished

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 // Caught Speeding CYU Asgn
+// 20 or more over speed limit = Big Ticket
+// In between 0-19 over speed limit = Small Ticket
+// 0 or lower over = No Ticket
+
+document.getElementById("btn").addEventListener("click", giveTicket);
+
+function giveTicket() {
+  let speed = +document.getElementById("speed").value;
+  let speedLimit = +document.getElementById("speedLimit").value;
+  let difference = speedLimit - speed;
+  if (difference >= 0) {
+    document.getElementById("output").innerHTML = "No Ticket";
+  } else if (difference < 0 && difference >= -19) {
+    document.getElementById("output").innerHTML = "Small Ticket";
+  } else {
+    document.getElementById("output").innerHTML = "Big Ticket";
+  }
+}
